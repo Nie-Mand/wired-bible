1658d492bb90798303ba16425b6a1104bdd2c5d5
Nie-Mand <chill.out.x19@gmail.com>
2024-01-14 20:23:22 +0100

    refactor: :zap: Improved Queries Performance to x24

--- src/services/index.ts
+++ src/services/index.ts
@@ -15,46 +15,61 @@ export async function chapters() {
 }
 
 export async function list(chapter: string) {
-  const keys = await kv.keys(`${chapter}:*:text`);
-  const ids = keys.map((key) => key.split(":")[1]);
+  const keys = await kv.keys(`${chapter}:*`);
+  const ids = keys
+    .filter((k) => k.endsWith("text"))
+    .map((key) => key.split(":")[1]);
 
-  let data: Practice[] = [];
-  for (const id of ids) {
-    const text = await kv.get<string>(`${chapter}:${id}:text`);
-    const tagsKeys = await kv.keys(`${chapter}:${id}:tags:*`);
-    const tags = tagsKeys[0].split(":")[3].split(",");
+  const chaptersKeys = keys
+    .filter((k) => k.endsWith("text"))
+    .map((key) => `${key.split(":")[0]}:${key.split(":")[1]}:text`);
+  const chapters = await kv.mget<string[]>(...chaptersKeys);
 
-    data.push({
-      chapter,
-      text: text || "",
-      tags,
-      id: parseInt(id),
-    });
-  }
+  const tags = keys
+    .filter((k) => !k.endsWith("text"))
+    .map((key) => key.split(":")[3].split(","));
+
+  const data: Practice[] = new Array(chapters.length).fill({}).map((_, i) => ({
+    chapter: keys[i].split(":")[0],
+    text: chapters[i],
+    tags: tags[i],
+    id: parseInt(ids[i]),
+  }));
 
   data.sort((a, b) => a.id! - b.id!);
-
   return data;
 }
 
 export async function tagged(tag: string) {
   const keys = await kv.keys(`*:*:*${tag}*`);
 
-  let data: Practice[] = [];
-  for (const key of keys) {
-    const id = key.split(":")[1];
-    const chapter = key.split(":")[0];
-    const text = await kv.get<string>(`${chapter}:${id}:text`);
-    const tagsKeys = await kv.keys(`*:${id}:tags:*`);
-    const tags = tagsKeys[0].split(":")[3].split(",");
+  const chaptersKeys = keys.map(
+    (key) => `${key.split(":")[0]}:${key.split(":")[1]}:text`
+  );
+  const chapters = await kv.mget<string[]>(...chaptersKeys);
 
-    data.push({
-      chapter,
-      text: text || "",
-      tags,
-      id: parseInt(id),
-    });
-  }
+  const tags = keys.map((key) => key.split(":")[3].split(","));
+
+  const data: Practice[] = new Array(chapters.length).fill({}).map((_, i) => ({
+    chapter: keys[i].split(":")[0],
+    text: chapters[i],
+    tags: tags[i],
+    id: parseInt(keys[i].split(":")[1]),
+  }));
+  // for (const key of keys) {
+  //   const id = key.split(":")[1];
+  //   const chapter = key.split(":")[0];
+  //   const text = await kv.get<string>(`${chapter}:${id}:text`);
+  //   const tagsKeys = await kv.keys(`*:${id}:tags:*`);
+  //   const tags = tagsKeys[0].split(":")[3].split(",");
+
+  //   data.push({
+  //     chapter,
+  //     text: text || "",
+  //     tags,
+  //     id: parseInt(id),
+  //   });
+  // }
 
   data.sort((a, b) => a.id! - b.id!);
 
